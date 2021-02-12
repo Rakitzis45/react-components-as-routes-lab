1b7f05e5edbc75fc288f871e51653241d25e38fc
Rakitzis45 <michael.rakitzis@gmail.com>
2021-02-12 12:36:07 -0500

    commit tree 7a341f1649fcfa66669d76cf994fc8e44cc0207d

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,14 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map((movie, index)=>
+          <div key={index}> {movie.title} <ul>
+            <li>Run time:{movie.time}</li>
+            <li>Genres <ul> {movie.genres.map((genre, index)=> <li key={index}>{genre}</li>)}</ul></li>
+            
+            </ul></div>
+        )}
     </div>
   );
 };
